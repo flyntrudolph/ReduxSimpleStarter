522805ff1c7e0f2f615ae91002aef69f2a5fb2aa
Flyntrudolph <606425+flyntrudolph@users.noreply.github.com>
2018-07-23 15:26:12 -0600

    fetch weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 const API_KEY = '9f87fd2d6437303a66945a75d2917f3a'
-
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-
-export function fetchWeather () {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`
   return {
     type: FETCH_WEATHER
   };
