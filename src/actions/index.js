9bb99f97e7f64dba0f55660829bff14b65814b11
Flyntrudolph <606425+flyntrudolph@users.noreply.github.com>
2018-07-23 15:17:52 -0600

    fetch weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,11 @@
 const API_KEY = '9f87fd2d6437303a66945a75d2917f3a'
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+
+export function fetchWeather () {
+  return {
+    type: FETCH_WEATHER
+  };
+}
