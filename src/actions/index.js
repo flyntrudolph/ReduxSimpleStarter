dc4b8d240337e77b93fdfa3f43475d4b33dcbc01
Flyntrudolph <606425+flyntrudolph@users.noreply.github.com>
2018-07-23 15:51:58 -0600

    adding axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,14 @@
+import axios from 'axios';
+
 const API_KEY = '9f87fd2d6437303a66945a75d2917f3a'
 const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
   return {
     type: FETCH_WEATHER
   };
