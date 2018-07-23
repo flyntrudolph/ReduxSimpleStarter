d146ba004d5a75ffa5affe908bacbca8c1911988
Flyntrudolph <606425+flyntrudolph@users.noreply.github.com>
2018-07-23 14:45:11 -0600

    searchbar near completion

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -4,23 +4,31 @@ export default class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: ''};
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
 }
 
 onInputChange(event) {
-  console.log(event.target.value);
+  this.setState({ term: event.target.value })
+}
+
+onFormSubmit(event) {
+  event.preventDefault();
 }
 
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit ={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite cities"
           className="form-control"
           value={this.state.term}
           onChange={this.onInputChange}/>
+        {/* if you have a callback, that references ".this", you probably
+        need to bind it "(this.onInputChange.bind(this)" */}
         <span className="input-group-btn">
-          <button type ="submit" className="btn btn-secondary">Submit</button>
+          <button type ="submit" className="btn btn-secondary">Flynt Rudolph</button>
         </span>
       </form>
     );
