c6bb67d5b1ef9eec45fdba2445f236655b04b752
Flyntrudolph <606425+flyntrudolph@users.noreply.github.com>
2018-07-23 23:39:50 -0600

    making lists of cities

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,5 +1,15 @@
-export default function(state = null, action) {
-  console.log('action received', action)
+import { FETCH_WEATHER } from '../actions/index';
 
+
+export default function(state = [], action) {
+  switch (action.type) {
+  case FETCH_WEATHER:
+      return [ action.payload.data, ...state];
+
+
+    //   break;
+    // default:
+
+  }
   return state;
 }
