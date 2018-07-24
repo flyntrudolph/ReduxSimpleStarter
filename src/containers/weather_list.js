f101752508e0f1d2f9ae858d408b886d6cea69b1
Flyntrudolph <606425+flyntrudolph@users.noreply.github.com>
2018-07-23 23:53:35 -0600

    making lists of cities

---
+++ src/containers/weather_list.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table table_hover">
+        <thead>
+          <tr>
+            <th>City</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+
+    );
+}
+}
